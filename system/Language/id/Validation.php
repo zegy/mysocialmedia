cc3a866e5ecdd15fd5c86c9dac6058846fb6f772
Zegy Sukmawan <zegy.sukmawan@gmail.com>
2022-08-08 12:41:52 +0700

    OTC Bahasa indonesia (add)

---
+++ system/Language/id/Validation.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Custom Bahasa Indonesia
+ */
+
+// Validation language settings
+return [
+	// Core Messages
+	 'min_length'            => '{field} harus berisi setidaknya {param} karakter.',
+	 'numeric'               => '{field} harus hanya berupa angka.',
+	 'required'              => '{field} diperlukan.',
+	 'valid_email'           => 'format {field} tidak sesuai.',
+	
+	// Files
+	 'uploaded'              => 'file {field} tidak valid.',
+	 'max_size'              => 'ukuran file {field} terlalu besar.',
+	 'is_image'              => 'file {field} tidak valid, harus berupa gambar.',
+	 'mime_in'               => 'tipe file gambar {field} tidak valid.',
+	 'max_dims'              => 'file {field} bukan gambar atau terlalu panjang dan lebar.',
+];

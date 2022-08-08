d578046928d51b7425c9df6942f4e36cdba876be
Zegy Sukmawan <zegy.sukmawan@gmail.com>
2022-08-08 23:10:11 +0700

    OTC IS THIS RIGHT?

--- app/Views/account/signup.php
+++ app/Views/account/signup.php
@@ -88,16 +88,10 @@
                     </div>
                 </div>
                 <div class="gender-details">
-                        <?php if (isset($prev_input)) { if ($prev_input['jenis_kelamin'] == 'm') { ?>
-                        <input type="radio" name="jenis_kelamin" value="m" id="dot-1" checked="checked">
-                        <input type="radio" name="jenis_kelamin" value="f" id="dot-2">
-                        <?php } else { ?>
-                        <input type="radio" name="jenis_kelamin" value="m" id="dot-1">
-                        <input type="radio" name="jenis_kelamin" value="f" id="dot-2" checked="checked">
-                        <?php } } else { ?>
-                        <input type="radio" name="jenis_kelamin" value="m" id="dot-1">
-                        <input type="radio" name="jenis_kelamin" value="f" id="dot-2">
-                        <?php } ?>
+                        <?php $checked = ('checked="checked"'); ?>
+                        <input type="radio" name="jenis_kelamin" value="m" id="dot-1" >
+                        <input type="radio" name="jenis_kelamin" value="f" id="dot-2" <?php echo $checked?>>
+                        
 
                     <span class="gender-title">Jenis Kelamin:</span>
                     <div class="category">

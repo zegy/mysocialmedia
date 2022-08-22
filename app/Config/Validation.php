c99a4ec4146fd75a1b0374fda63d06a59557f47a
Zegy Sukmawan <zegy.sukmawan@gmail.com>
2022-08-23 01:04:57 +0700

    OTC

--- app/Config/Validation.php
+++ app/Config/Validation.php
@@ -44,13 +44,31 @@ class Validation
     // ZEGY OTC CUSTOM
     // https://codeigniter4.github.io/CodeIgniter4/libraries/validation.html#how-to-save-your-rules
     public $createAccount = [
-        'nama_lengkap'        => 'required|min_length[3]',
-        'username'            => 'required|min_length[5]|alpha_dash|is_unique[t_user.user_name]',
-        'email'               => 'required|valid_email|is_unique[t_user.user_email]',
-        'nomor_handphone'     => 'required|min_length[8]|numeric',
-        'password'            => 'required|min_length[8]',
-        'konfirmasi_password' => 'required|matches[password]',
-        'profile_img'         => 'uploaded[profile_img]|is_image[profile_img]|mime_in[profile_img,image/jpg,image/jpeg]|max_size[profile_img,30]|max_dims[profile_img,200,200]',
+        'nama_lengkap' => [
+            'label' => 'nama lengkap',
+            'rules' => 'required|min_length[3]'
+        ],
+        'username' => [
+            'label' => 'username',
+            'rules' => 'required|min_length[5]|alpha_dash|is_unique[t_user.user_name]'
+        ],
+        'email' => [
+            'label' => 'email',
+            'rules' => 'required|valid_email|is_unique[t_user.user_email]'
+        ],
+        'nomor_handphone' => [
+            'label' => 'nomor handphone',
+            'rules' => 'required|min_length[8]|numeric'
+        ],
+        'password' => [
+            'label' => 'password',
+            'rules' => 'required|min_length[8]'
+        ],
+        'konfirmasi_password' => [
+            'label' => 'konfirmasi password',
+            'rules' => 'required|matches[password]'
+        ],
+        'profile_img' => 'uploaded[profile_img]|is_image[profile_img]|mime_in[profile_img,image/jpg,image/jpeg]|max_size[profile_img,30]|max_dims[profile_img,200,200]',
         'bio'                 => 'required|max_length[250]',
         'jenis_kelamin'       => 'required'
     ];

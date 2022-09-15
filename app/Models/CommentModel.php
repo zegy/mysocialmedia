caf1a09fe8340dfdd9cc82a102ecef213aec99b8
Zegy Sukmawan <zegy.sukmawan@gmail.com>
2022-09-16 02:16:39 +0700

    Clean other C/M based on post C/M

--- app/Models/CommentModel.php
+++ app/Models/CommentModel.php
@@ -8,7 +8,7 @@ class CommentModel extends Model
 {
     protected $table         = 't_comment';
     protected $primaryKey    = 'comment_pk';
-    protected $returnType    = 'object'; // ZEGY OTC : global return here, no individual one?
+    protected $returnType    = 'object';
     protected $allowedFields =
     [
         'comment_fk_user',
@@ -16,17 +16,22 @@ class CommentModel extends Model
         'comment_text',
         'comment_date_time'
     ];
+    protected $useTimestamps = true;
+    protected $createdField  = 'comment_date_time';
+    protected $updatedField; //NOTE It's needed by "useTimestamps" even if we not use it. https://codeigniter.com/user_guide/models/model.html?highlight=find#usetimestamps
+
+    protected $select = '
+        comment_pk           as cid,
+        comment_text         as texto,
+        comment_date_time    as data,
+        user_pk              as uid,
+        user_full_name       as nome,
+        user_profile_picture as image
+    ';
 
     public function getAllByPost($pid)
     {
-        return $this->select('
-                                comment_pk           as cid,
-                                comment_text         as texto,
-                                comment_date_time    as data,
-                                user_pk              as uid,
-                                user_full_name       as nome,
-                                user_profile_picture as image
-                            ')
+        return $this->select($this->select)
                     ->join('t_post', 'post_pk = comment_fk_post')
                     ->join('t_user', 'comment_fk_user = user_pk')
                     ->where('post_pk', $pid)
@@ -35,18 +40,18 @@ class CommentModel extends Model
 
     public function getAllByKeyword($keyword)
     {
-        return $this->select('
-                                comment_pk           as cid,
-                                comment_text         as texto,
-                                comment_date_time    as data,
-                                user_pk              as uid,
-                                user_full_name       as nome,
-                                user_profile_picture as image,
-                                post_pk              as pid
-                            ')
+        return $this->select($this->select . 'post_pk as pid')
                     ->join('t_post', 'post_pk = comment_fk_post')
                     ->join('t_user', 'comment_fk_user = user_pk')
                     ->like('comment_text', $keyword)
                     ->findAll();
     }
 }
+
+/*NOTE
+    The Model does not provide a perfect interface to the Query Builder.
+    The Model and the Query Builder are separate classes with different purposes.
+    Example : "select()" and "join()" is part of Query Builder, "find()" and "paginate()" is part of Model.
+    Query Builder methods and Model’s CRUD methods can be in the same chained call.
+    https://codeigniter.com/user_guide/models/model.html?highlight=find#working-with-query-builder
+*/

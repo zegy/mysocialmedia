7d0ae17a17202cb896c66cfa10b58dc18ec88be0
Zegy Sukmawan <zegy.sukmawan@gmail.com>
2022-09-17 13:00:14 +0700

    Add stila template (OTC)

---
+++ app/Views/layouts/js.php
@@ -0,0 +1,20 @@
+  <!-- General JS Scripts -->
+  <script src="<?php echo base_url(); ?>assets/modules/jquery.min.js"></script>
+  <script src="<?php echo base_url(); ?>assets/modules/popper.js"></script>
+  <script src="<?php echo base_url(); ?>assets/modules/tooltip.js"></script>
+  <script src="<?php echo base_url(); ?>assets/modules/bootstrap/js/bootstrap.min.js"></script>
+  <script src="<?php echo base_url(); ?>assets/modules/nicescroll/jquery.nicescroll.min.js"></script>
+  <script src="<?php echo base_url(); ?>assets/modules/moment.min.js"></script>
+  <script src="<?php echo base_url(); ?>assets/js/stisla.js"></script>
+  
+  <!-- JS Libraies SELECTABLE : features_posts-->
+  <script src="<?php echo base_url(); ?>assets/modules/jquery-selectric/jquery.selectric.min.js"></script>
+
+  <!-- Page Specific JS File SELECTABLE : features_posts-->
+  <script src="<?php echo base_url(); ?>assets/js/page/features-posts.js"></script>
+
+  <!-- Template JS File -->
+  <script src="<?php echo base_url(); ?>assets/js/scripts.js"></script>
+  <script src="<?php echo base_url(); ?>assets/js/custom.js"></script>
+</body>
+</html>

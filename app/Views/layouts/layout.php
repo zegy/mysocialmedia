d5ade81ad079b106eb305d4f811fa441cda1a0df
Zegy Sukmawan <zegy.sukmawan@gmail.com>
2022-09-19 21:56:49 +0700

    OTC (Add" form add post")

--- app/Views/layouts/layout.php
+++ app/Views/layouts/layout.php
@@ -11,7 +11,12 @@
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
 
   <!-- CSS Libraries -->
-  <link rel="stylesheet" href="../node_modules/selectric/public/selectric.css">
+    <!-- features-posts -->
+    <link rel="stylesheet" href="../node_modules/selectric/public/selectric.css">
+    <!-- features-post-create TODO need to check!-->
+    <link rel="stylesheet" href="../node_modules/summernote/dist/summernote-bs4.css">
+    <!-- <link rel="stylesheet" href="../node_modules/selectric/public/selectric.css"> -->
+    <link rel="stylesheet" href="../node_modules/bootstrap-tagsinput/dist/bootstrap-tagsinput.css">
 
   <!-- Template CSS -->
   <link rel="stylesheet" href="../assets/css/style.css">
@@ -424,22 +429,31 @@
   </div>
 
   <!-- General JS Scripts -->
-  <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.nicescroll/3.7.6/jquery.nicescroll.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
-  <script src="../assets/js/stisla.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.nicescroll/3.7.6/jquery.nicescroll.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
+    <script src="../assets/js/stisla.js"></script>
 
   <!-- JS Libraies -->
-  <script src="../node_modules/selectric/public/jquery.selectric.min.js"></script>
+    <!-- features-posts -->
+    <script src="../node_modules/selectric/public/jquery.selectric.min.js"></script>
+    <!-- features-post-create TODO need to check!-->
+    <script src="../node_modules/summernote/dist/summernote-bs4.js"></script>
+    <script src="../node_modules/selectric/public/jquery.selectric.min.js"></script>
+    <script src="../node_modules/jquery_upload_preview/assets/js/jquery.uploadPreview.min.js"></script>
+    <script src="../node_modules/bootstrap-tagsinput/dist/bootstrap-tagsinput.min.js"></script>
 
   <!-- Template JS File -->
   <script src="../assets/js/scripts.js"></script>
   <script src="../assets/js/custom.js"></script>
 
   <!-- Page Specific JS File -->
-  <script src="../assets/js/page/features-posts.js"></script>
+    <!-- features-posts -->
+    <script src="../assets/js/page/features-posts.js"></script>
+    <!-- features-post-create -->
+    <script src="../assets/js/page/features-post-create.js"></script>
 
   <!-- TODO Custom (Future : move to "custom.js"-->
   <script>

bd9c18b4800384a83fae4c5cda0897c4e8068876
Zegy Sukmawan <zegy.sukmawan@gmail.com>
2022-09-24 20:49:16 +0700

    Fixed new stisla pagination

--- app/Views/home.php
+++ app/Views/home.php
@@ -72,31 +72,7 @@
                 </table>
               </div>
               <div class="float-right">
-                <!-- <nav>
-                  <ul class="pagination">
-                    <li class="page-item disabled">
-                      <a class="page-link" href="#" aria-label="Previous">
-                        <span aria-hidden="true">&laquo;</span>
-                        <span class="sr-only">Previous</span>
-                      </a>
-                    </li>
-                    <li class="page-item active">
-                      <a class="page-link" href="#">1</a>
-                    </li>
-                    <li class="page-item">
-                      <a class="page-link" href="#">2</a>
-                    </li>
-                    <li class="page-item">
-                      <a class="page-link" href="#">3</a>
-                    </li>
-                    <li class="page-item">
-                      <a class="page-link" href="#" aria-label="Next">
-                        <span aria-hidden="true">&raquo;</span>
-                        <span class="sr-only">Next</span>
-                      </a>
-                    </li>
-                  </ul>
-                </nav> -->
+                <?php echo $pager->links('default', 'stisla') ?>
               </div>
             </div>
           </div>

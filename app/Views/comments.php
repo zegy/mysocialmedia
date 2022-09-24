3a0c0b658a58f92923a0f61e5140ed49ff264854
Zegy Sukmawan <zegy.sukmawan@gmail.com>
2022-09-24 20:05:53 +0700

    Clean comment view

--- app/Views/comments.php
+++ app/Views/comments.php
@@ -25,44 +25,9 @@
             <div class="card-body">
               <ul class="list-unstyled list-unstyled-border list-unstyled-noborder">
                 <li class="media">
-                  <img alt="image" class="mr-3 rounded-circle" width="70" src="../assets/img/avatar/avatar-1.png">
-                  <div class="media-body">
-                    <div class="media-right"><div class="text-primary">Approved</div></div>
-                    <div class="media-title mb-1">Rizal Fakhri</div>
-                    <div class="text-time">Yesterday</div>
-                    <div class="media-description text-muted">Duis aute irure dolor in reprehenderit in voluptate velit esse
-                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
-                    <div class="media-links">
-                      <a href="#">View</a>
-                      <div class="bullet"></div>
-                      <a href="#">Edit</a>
-                      <div class="bullet"></div>
-                      <a href="#" class="text-danger">Trash</a>
-                    </div>
-                  </div>
-                </li>
-                <li class="media">
-                  <img alt="image" class="mr-3 rounded-circle" width="70" src="../assets/img/avatar/avatar-2.png">
-                  <div class="media-body">
-                    <div class="media-right"><div class="text-warning">Pending</div></div>
-                    <div class="media-title mb-1">Bambang Uciha</div>
-                    <div class="text-time">Yesterday</div>
-                    <div class="media-description text-muted">Duis aute irure dolor in reprehenderit in voluptate velit esse
-                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
-                    <div class="media-links">
-                      <a href="#">View</a>
-                      <div class="bullet"></div>
-                      <a href="#">Edit</a>
-                      <div class="bullet"></div>
-                      <a href="#" class="text-danger">Trash</a>
-                    </div>
-                  </div>
-                </li>
-                <li class="media">
                   <img alt="image" class="mr-3 rounded-circle" width="70" src="../assets/img/avatar/avatar-3.png">
                   <div class="media-body">
+                    <!-- <div class="media-right"><div class="text-warning">Pending</div></div> -->
                     <div class="media-right"><div class="text-primary">Approved</div></div>
                     <div class="media-title mb-1">Ujang Maman</div>
                     <div class="text-time">Yesterday</div>

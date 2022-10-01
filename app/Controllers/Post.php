7b81a9c893a01840c02777895662ba08d42621df
Zegy Sukmawan <zegy.sukmawan@gmail.com>
2022-10-01 20:03:01 +0700

    Clean post C (prep for using modal)

--- app/Controllers/Post.php
+++ app/Controllers/Post.php
@@ -7,35 +7,15 @@ use App\Models\PostModel;
 
 class Post extends BaseController
 {
-    function __construct() //TODO no "public/protected"?
+    function __construct()
     {
         helper('form');
         $this->postModel = new PostModel();
     }
 
-    protected function checkOwnership($pid)
-    {
-        $post = $this->postModel->find($pid);
-        
-        if ((empty($post)) or (session('id') != $post->post_fk_user))
-        {
-            throw \CodeIgniter\Exceptions\PageNotFoundException::forPageNotFound();
-        }
-        else
-        {
-            return $post;
-        }
-    }
-
-    public function saveForm()
-    {
-        return view('forms/add_post');
-    }
-
     public function save()
     {
-        $data = $this->request->getPost();
-        // dd($data);
+        $data = $this->request->getPost(); //GET title, text, type
         $dataToSave =
         [
             "post_fk_user" => session('id'),
@@ -44,45 +24,23 @@ class Post extends BaseController
             "post_type"    => $data['post_type']
         ];
         $this->postModel->insert($dataToSave); //NOTE In case using "save()", if it contain PK then it update the existing record or else it insert into the database (no need to create "update" method)
-        return redirect()->to('/');
+        return redirect()->back();
     }
 
-    // public function update($pid)
     public function update()
     {   
-        // $data = $this->request->getPost();
-        // $postData = $this->checkOwnership($pid);
-
-        // if (empty($data)) //NOTE Show update form (single route, updateForm() is removed)
-        // {
-        //     echo view('forms/form_edit_post', ['post' => $postData]);
-        // }
-        // else
-        // {
-        //     $dataToSave =
-        //     [
-        //         "post_text" => $data["text"]
-        //     ];
-        //     $this->postModel->update($pid, $dataToSave);        
-        //     return redirect()->to('/');
-        // }
-
-        $data = $this->request->getPost();
-        $this->checkOwnership($data["pid"]);
+        $data = $this->request->getPost(); //GET pid, text
         $dataToSave =
         [
             "post_text" => $data["text"]
         ];
         $this->postModel->update($data["pid"], $dataToSave);        
-        // return redirect()->to('/');
-        return redirect()->back();         
+        return redirect()->back();
     }
 
-    // public function delete($pid)
     public function delete()
     {
-        $data = $this->request->getPost();
-        $this->checkOwnership($data["pid"]);
+        $data = $this->request->getPost(); //GET pid
         $this->postModel->delete($data["pid"]);
         return redirect()->back();
     }
@@ -108,28 +66,7 @@ class Post extends BaseController
 
 
 
-    // public function updateForm($pid)
-    // {
-    //     session()->keepFlashdata('curPage'); // preserve flashdata from "home" to "update" method.
-    //     $postData = $this->checkOwnership($pid);
-    //     echo view('forms/form_edit_post', ['post' => $postData]);
-    // }
-
-    // ZEGY OTC IF C CALL C (NOT DIRECT FROM UNMATCHED MODEL)
-    // public function getAllByType($type)
-    // {
-    //     return $this->postModel->getAllByType($type);
-    // }
-
-    // public function getAllByKeyword($keyword)
-    // {
-    //     return $this->postModel->getAllByKeyword($keyword);
-    // }
-
-    // public function getAllByUser($user)
-    // {
-    //     return $this->postModel->getAllByUser($user);
-    // }
+    
 
     // =================================== ZEGY DEVELOPMENT ONLY ===================================
     public function saveBatchPublic()

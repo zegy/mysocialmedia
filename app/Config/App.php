bcf00f5fd040247b07374655b90ac31d2b2c6042
Zegy Sukmawan <zegy.sukmawan@gmail.com>
2022-10-02 23:17:32 +0700

    Problem (fix in future "fresh" CI"). Readme
    root is already set in "public" folder. How?

--- app/Config/App.php
+++ app/Config/App.php
@@ -23,7 +23,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-      public $baseURL = 'https://mysocialmedia.test/public';
+      public $baseURL = 'https://mysocialmedia.test/';
 
     /**
      * --------------------------------------------------------------------------

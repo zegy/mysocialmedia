977e7002a1889d9481b617c0d947030739d4db9a
Zegy Sukmawan <zegy.sukmawan@gmail.com>
2022-10-02 23:10:52 +0700

    Update login.php

--- app/Views/login.php
+++ app/Views/login.php
@@ -8,11 +8,11 @@
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="<?= base_url('plugins/fontawesome-free/css/all.min.css') ?>">
+  <link rel="stylesheet" href="<?= base_url('assets/plugins/fontawesome-free/css/all.min.css') ?>">
   <!-- icheck bootstrap -->
-  <link rel="stylesheet" href="<?= base_url('plugins/icheck-bootstrap/icheck-bootstrap.min.css') ?>">
+  <link rel="stylesheet" href="<?= base_url('assets/plugins/icheck-bootstrap/icheck-bootstrap.min.css') ?>">
   <!-- Theme style -->
-  <link rel="stylesheet" href="<?= base_url('dist/css/adminlte.min.css') ?>">
+  <link rel="stylesheet" href="<?= base_url('assets/dist/css/adminlte.min.css') ?>">
 </head>
 <body class="hold-transition login-page">
 <div class="login-box">
@@ -82,10 +82,10 @@
 <!-- /.login-box -->
 
 <!-- jQuery -->
-<script src="<?= base_url('plugins/jquery/jquery.min.js') ?>"></script>
+<script src="<?= base_url('assets/plugins/jquery/jquery.min.js') ?>"></script>
 <!-- Bootstrap 4 -->
-<script src="<?= base_url('plugins/bootstrap/js/bootstrap.bundle.min.js') ?>"></script>
+<script src="<?= base_url('assets/plugins/bootstrap/js/bootstrap.bundle.min.js') ?>"></script>
 <!-- AdminLTE App -->
-<script src="<?= base_url('dist/js/adminlte.min.js') ?>"></script>
+<script src="<?= base_url('assets/dist/js/adminlte.min.js') ?>"></script>
 </body>
 </html>

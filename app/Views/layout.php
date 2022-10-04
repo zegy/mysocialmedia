ca2cc3b9c6084eda3f78caae61cb7e9fca3351dd
Zegy Sukmawan <zegy.sukmawan@gmail.com>
2022-10-04 13:43:07 +0700

    OTC layout

--- app/Views/layout.php
+++ app/Views/layout.php
@@ -32,23 +32,9 @@
       <li class="nav-item">
         <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
       </li>
-      <li class="nav-item d-none d-sm-inline-block">
-        <a href="index3.html" class="nav-link">Home</a>
-      </li>
-      <li class="nav-item d-none d-sm-inline-block">
-        <a href="#" class="nav-link">Contact</a>
-      </li>
-      <!-- TODO Custom START -->
-      <li class="nav-item d-none d-sm-inline-block">
-        <a href="<?php echo base_url('login/signout') ?>" class="nav-link">Sign Out</a>
-      </li>
-      <li class="nav-item d-none d-sm-inline-block">
-        <a href="<?php echo base_url('post/publicposts') ?>" class="nav-link">Post Public</a>
-      </li>
-      <li class="nav-item d-none d-sm-inline-block">
+      <li class="nav-item">
         <a href="<?php echo base_url('user/showprofile/' . session('id')) ?>" class="nav-link">Profile</a>
       </li>
-      <!-- TODO Custom END -->
     </ul>
 
     <!-- Right navbar links -->

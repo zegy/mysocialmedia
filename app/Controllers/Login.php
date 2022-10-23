5d9c32851f7e52cce7a440705e5145c349707512
Zegy Sukmawan <zegy.sukmawan@gmail.com>
2022-10-23 19:35:47 +0700

    OTC ajax login

--- app/Controllers/Login.php
+++ app/Controllers/Login.php
@@ -26,35 +26,62 @@ class Login extends BaseController
 
     public function signIn()
     {
-        $data = $this->request->getPost();
-        // dd($data);
-
-        $userModel = new UserModel();
-        $dataUser = $userModel->where('user_email', $data['email'])->first();
-
-        if (!empty($dataUser))
-        {
-            $hash = $dataUser->user_password;
-
-            if (password_verify($data['password'], $hash))
-            {
-                session()->set('isLoggedIn', true);
-                session()->set('id', $dataUser->user_pk);
-                session()->set('role', $dataUser->user_role);
-                session()->set('picture', $dataUser->user_profile_picture);
-                return redirect()->to('/fordis/umum'); //TODO right way?
-            }
-            else
-            {
-               session()->setFlashData('msg','Usuario ou senha incorretos!');
-               return redirect()->back();         
-            }
+        $rules = [
+          'email' => 'required',
+          'password' => 'required',
+        ];
+    
+        if (!$this->validate($rules)) {
+          $errors = [
+            'email' => $this->validation->getError('email'),
+            'password' => $this->validation->getError('password'),
+          ];
+    
+          $output = [
+            'status' => FALSE,
+            'errors' => $errors
+          ];
+          echo json_encode($output);
+        } else {
+        //   $this->model->save([
+        //     'name' => $this->request->getPost('name'),
+        //     'price' => $price,
+        //     'category' => $this->request->getPost('category'),
+        //     'detail' => $this->request->getPost('detail'),
+        //   ]);
+          echo json_encode(['status' => TRUE]);
         }
-        else
-        {
-            session()->setFlashData('msg','Usuario ou senha incorretos!');
-            return redirect()->back();         
-        }
+
+        // ========================================================================================
+        // $data = $this->request->getPost();
+        // // dd($data);
+
+        // $userModel = new UserModel();
+        // $dataUser = $userModel->where('user_email', $data['email'])->first();
+
+        // if (!empty($dataUser))
+        // {
+        //     $hash = $dataUser->user_password;
+
+        //     if (password_verify($data['password'], $hash))
+        //     {
+        //         session()->set('isLoggedIn', true);
+        //         session()->set('id', $dataUser->user_pk);
+        //         session()->set('role', $dataUser->user_role);
+        //         session()->set('picture', $dataUser->user_profile_picture);
+        //         return redirect()->to('/fordis/umum'); //TODO right way?
+        //     }
+        //     else
+        //     {
+        //        session()->setFlashData('msg','Usuario ou senha incorretos!');
+        //        return redirect()->back();         
+        //     }
+        // }
+        // else
+        // {
+        //     session()->setFlashData('msg','Usuario ou senha incorretos!');
+        //     return redirect()->back();         
+        // }
     }
 
     public function signOut()
--- app/Views/login.php
+++ app/Views/login.php
@@ -8,6 +8,9 @@
   <!-- Google Font: Source Sans Pro --> <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome --> <link rel="stylesheet" href="<?= base_url('assets/plugins/fontawesome-free/css/all.min.css') ?>">
   <!-- Theme style --> <link rel="stylesheet" href="<?= base_url('assets/dist/css/adminlte.min.css') ?>">
+  
+  <!-- OTC from ajax ci4 example -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/11.1.4/sweetalert2.min.css" integrity="sha512-Ls19wNglxCDcP78k23k5MygHFHAamARZWDggNovFF3XM4nFTgJz28wBM3m76/bqxvaGWvLKwsv/toFoLqhF8Gg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 
 <body class="hold-transition login-page">
@@ -18,7 +21,7 @@
       </div>
       <div class="card-body">
         <p class="login-box-msg">Sign in to start your session</p>
-        <form action="<?php echo base_url('login/signin') ?>" method="post">
+        <?= form_open('login/signin', ['id' => 'form-data']) ?>
           <div class="input-group mb-3">
             <input type="email" class="form-control" name="email" placeholder="Email">
             <div class="input-group-append">
@@ -48,7 +51,7 @@
               <button type="submit" class="btn btn-primary btn-block">Sign In</button>
             </div><!-- /.col -->
           </div><!-- /.row -->
-        </form>
+        <?= form_close() ?>
       </div><!-- /.card-body -->
     </div><!-- /.card -->
   </div><!-- /.login-box -->
@@ -57,5 +60,60 @@
   <!-- jQuery --> <script src="<?= base_url('assets/plugins/jquery/jquery.min.js') ?>"></script>
   <!-- Bootstrap 4 --> <script src="<?= base_url('assets/plugins/bootstrap/js/bootstrap.bundle.min.js') ?>"></script>
   <!-- AdminLTE App --> <script src="<?= base_url('assets/dist/js/adminlte.min.js') ?>"></script>
+
+  <!-- OTC from ajax ci4 example -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/11.1.4/sweetalert2.min.js" integrity="sha512-Lbwer45RtGISU+efaUoil1EFYFliqkKOaZhUMXG8RoZZ5fdjpK4S/2khwZynw8vyItDeaRZ+IE6XdKA6XCsyxQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+  <script>
+    const Toast = Swal.mixin({
+      toast: true,
+      position: 'top-end',
+      showConfirmButton: false,
+      timer: 2000,
+      timerProgressBar: true,
+    })
+
+    $(document).ready(function() {
+      $(document).on("submit", "#form-data", function(e) {
+        e.preventDefault()
+
+        $.ajax({
+          url: $(this).attr("action"),
+          type: $(this).attr("method"),
+          data: $(this).serialize(),
+          dataType: "json",
+          success: function(res) {
+            if (res.status) {
+            //   $(".modal").modal("toggle")
+              Toast.fire({
+                icon: 'success',
+                title: 'Data berhasil ditambah'
+              })
+              source_data()
+            } else {
+              $.each(res.errors, function(key, value) {
+                $('[name="' + key + '"]').addClass('is-invalid')
+                // $('[name="' + key + '"]').next().text(value)
+                if (value == "") {
+                  $('[name="' + key + '"]').removeClass('is-invalid')
+                  $('[name="' + key + '"]').addClass('is-valid')
+                }
+              })
+            }
+          }
+        })
+
+        $("#form-data input").on("keyup", function() {
+          $(this).removeClass('is-invalid is-valid')
+        })
+        $("#form-data input").on("click", function() {
+          $(this).removeClass('is-invalid is-valid')
+        })
+        $("#form-data select").on("click", function() {
+          $(this).removeClass('is-invalid is-valid')
+        })
+      })
+    })
+  </script>
 </body>
 </html>

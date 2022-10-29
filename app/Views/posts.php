e4c91399d528ce6209be0df70e905ca6c3ff2c6c
Zegy Sukmawan <zegy.sukmawan@gmail.com>
2022-10-29 21:24:00 +0700

    OTC New post button

--- app/Views/posts.php
+++ app/Views/posts.php
@@ -5,10 +5,11 @@
   <section class="content-header"><!-- Content Header (Page header) -->
     <div class="container-fluid">
       <div class="row mb-2">
-        <div class="col-sm-6">
-          <h1>Simple Tables</h1>
+        <div class="col-sm-9">
+          <h1 class="float-left">Simple Tables</h1>
+          <button style="margin-left: 10px;" class="btn btn-primary float-left"><i class="fa fa-plus"></i> Buat Diskusi</button>
         </div>
-        <div class="col-sm-6">
+        <div class="col-sm-3">
           <ol class="breadcrumb float-sm-right">
             <li class="breadcrumb-item"><a href="#">Home</a></li>
             <li class="breadcrumb-item active">Simple Tables</li>

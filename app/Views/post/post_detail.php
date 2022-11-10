fff45fd9a0acef71d80be1f9918e10fbe214c92d
Zegy Sukmawan <zegy.sukmawan@gmail.com>
2022-11-10 14:10:54 +0700

    add timer before redirect

--- app/Views/post/post_detail.php
+++ app/Views/post/post_detail.php
@@ -117,6 +117,10 @@
 <!-- ================================================ SCRIPTS ================================================ -->
 <script> //NOTE Inside this "$(document).ready(function)", this "script" tag is not needed, it just only to make it readable
   <?= $this->section('script') ?>
+  function redirect() {
+    window.location = "<?= base_url('group/' . $post->type) ?>" //TODO this is for redirect (since it's not working via C. But is it the best way?
+  }
+
   $(document).on("click", ".btn-delete-post", function() {
     var item_id = $(this).data("id") //TODO LEARN DATA-ID
 
@@ -145,7 +149,7 @@
       success: function(res) {
         if (res.status) {
           $(".modal").modal("toggle")
-          window.location = "<?= base_url('group/' . $post->type) ?>" //TODO this is for redirect (since it's not working via C. But is it the best way?
+          setTimeout(redirect, 1000)
         }
       }
     })

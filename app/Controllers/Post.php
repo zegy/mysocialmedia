cfba9febb39b10fe65e709e3d4623c7565bcefc4
Zegy Sukmawan <zegy.sukmawan@gmail.com>
2022-11-17 13:49:18 +0700

    Clean

--- app/Controllers/Post.php
+++ app/Controllers/Post.php
@@ -88,87 +88,40 @@ class Post extends BaseController
         }
     }
 
-    public function save () //NOTE : Single create + update function
-    {    
-        $validated = $this->validate([
-            'judul'     => ['required'],
-            'deskripsi' => ['required'],
-            'files'     => [ //TODO : set max 5! Problem with file names in SQL
-                'mime_in[files,image/jpg,image/jpeg,image/gif,image/png]',
-                'max_size[files,4096]',
-            ]
-        ]);
-
-        if (!$validated)
+    public function save () //NOTE : AJAX. Single create + update function
+    {
+        if ($this->request->isAJAX())
         {
-            $errors = [ //NOTE : "getErrors()" did not return input field that "valid", hence the "Getting a Single Error" used instead.
-                'judul'     => $this->validation->getError('judul'),
-                'deskripsi' => $this->validation->getError('deskripsi'),
-                'files'     => $this->validation->getError('files') //TODO (pending) : individual "error" for each file
-            ];
+            $validated = $this->validate([
+                'judul'     => ['required'],
+                'deskripsi' => ['required'],
+                'files'     => [ //TODO : set max 5! Problem with file names in SQL
+                    'mime_in[files,image/jpg,image/jpeg,image/gif,image/png]',
+                    'max_size[files,4096]',
+                ]
+            ]);
 
-            $output = [
-                'errors' => $errors,
-                'status' => FALSE
-            ];
-
-            echo json_encode($output);
-        }
-        else
-        {
-            $data = null; //TODO : need this?
-            $type = $this->request->getPost('type'); //NOTE : To decide it's New / Edit post
-            if ($type == 'add') //NOTE : TYPE = ADD
+            if (!$validated) //NOTE : IF NOT VALID = return error array with the key and value for each input (only key with empty value for input with no error)
             {
-                $files = $this->request->getFileMultiple('files');
-                $count = 0;
-                $fileNames[] = null; //NOTE (pending) : empty($files) is not working for file input to be optional. Solved (weirdly) by give "$fileNamesString" no value to begin with.
-                foreach($files as $file)
-                {
-                    if($file->isValid() && !$file->hasMoved())
-                    {
-                        $name = $file->getRandomName();
-                        $fileNames[$count] = $name; 
-                        $file->move(WRITEPATH . 'uploads/posts', $name);
-                        $count++;
-                    }           
-                }
-
-                $fileNamesString = implode(",", $fileNames); 
-
-                $data = [
-                    "post_fk_user" => session('id'),
-                    "post_title"   => $this->request->getPost('judul'),
-                    "post_text"    => $this->request->getPost('deskripsi'),
-                    "post_type"    => $this->request->getPost('group'),
-                    "post_img"     => $fileNamesString
+                $errors = [ //NOTE : "getErrors()" did not return input field that "valid", hence the "Getting a Single Error" used instead.
+                    'judul'     => $this->validation->getError('judul'),
+                    'deskripsi' => $this->validation->getError('deskripsi'),
+                    'files'     => $this->validation->getError('files') //TODO (pending) : individual "error" for each file
                 ];
+
+                $output = [
+                    'errors' => $errors,
+                    'status' => false
+                ];
+
+                echo json_encode($output);
             }
-            else //NOTE : TYPE = EDIT
+            else //NOTE : IF VALID
             {
-                $pid = $this->request->getPost('pid'); //NOTE : Need for "pid_redirect"
-
-                $data = [
-                    "post_pk"      => $pid,
-                    "post_fk_user" => session('id'),
-                    "post_title"   => $this->request->getPost('judul'),
-                    "post_text"    => $this->request->getPost('deskripsi'),
-                    "post_type"    => $this->request->getPost('group'),
-                ];
-                
-                $check_replace_files = $this->request->getPost('exampleCheck1');
-                if ($check_replace_files == true)
+                $data = null; //TODO : need this?
+                $type = $this->request->getPost('type'); //NOTE : To decide if it's create or update post
+                if ($type == 'add') //NOTE : TYPE = ADD
                 {
-                    $old_files = $this->request->getPost('old_files'); //NOTE : String    
-                    if (!empty($old_files))
-                    {
-                        $old_files_deletes = explode(",", $old_files);
-                        foreach ($old_files_deletes as $old_files_delete)
-                        {
-                            unlink(WRITEPATH . 'uploads/posts/' . $old_files_delete);
-                        }
-                    }
-                    
                     $files = $this->request->getFileMultiple('files');
                     $count = 0;
                     $fileNames[] = null; //NOTE (pending) : empty($files) is not working for file input to be optional. Solved (weirdly) by give "$fileNamesString" no value to begin with.
@@ -182,20 +135,74 @@ class Post extends BaseController
                             $count++;
                         }           
                     }
-                    $fileNamesString = implode(",", $fileNames); 
-                
-                    $data["post_img"] = $fileNamesString;
-                }
-            }
-            
-            $this->postModel->save($data);
-            $pid_redirect = $pid ?? $this->postModel->insertID(); //NOTE : Get id from the last insert/save (if new post). TODO : What if other user do the insert/save?
 
-            echo json_encode([
-                'group'  => $this->request->getPost('group'),
-                'pid'    => $pid_redirect,
-                'status' => TRUE
-            ]);
+                    $fileNamesString = implode(",", $fileNames); 
+
+                    $data = [
+                        "post_fk_user" => session('id'),
+                        "post_title"   => $this->request->getPost('judul'),
+                        "post_text"    => $this->request->getPost('deskripsi'),
+                        "post_type"    => $this->request->getPost('group'),
+                        "post_img"     => $fileNamesString
+                    ];
+                }
+                else //NOTE : TYPE = EDIT
+                {
+                    $pid = $this->request->getPost('pid'); //NOTE : Need for "pid_redirect"
+
+                    $data = [
+                        "post_pk"      => $pid,
+                        "post_fk_user" => session('id'),
+                        "post_title"   => $this->request->getPost('judul'),
+                        "post_text"    => $this->request->getPost('deskripsi'),
+                        "post_type"    => $this->request->getPost('group'),
+                    ];
+                    
+                    $check_replace_files = $this->request->getPost('exampleCheck1');
+                    if ($check_replace_files == true)
+                    {
+                        $old_files = $this->request->getPost('old_files'); //NOTE : String    
+                        if (!empty($old_files))
+                        {
+                            $old_files_deletes = explode(",", $old_files);
+                            foreach ($old_files_deletes as $old_files_delete)
+                            {
+                                unlink(WRITEPATH . 'uploads/posts/' . $old_files_delete);
+                            }
+                        }
+                        
+                        $files = $this->request->getFileMultiple('files');
+                        $count = 0;
+                        $fileNames[] = null; //NOTE (pending) : empty($files) is not working for file input to be optional. Solved (weirdly) by give "$fileNamesString" no value to begin with.
+                        foreach($files as $file)
+                        {
+                            if($file->isValid() && !$file->hasMoved())
+                            {
+                                $name = $file->getRandomName();
+                                $fileNames[$count] = $name; 
+                                $file->move(WRITEPATH . 'uploads/posts', $name);
+                                $count++;
+                            }           
+                        }
+                        $fileNamesString = implode(",", $fileNames); 
+                    
+                        $data["post_img"] = $fileNamesString;
+                    }
+                }
+                
+                $this->postModel->save($data);
+                $pid_redirect = $pid ?? $this->postModel->insertID(); //NOTE : Get id from the last insert/save (if new post). TODO : What if other user do the insert/save?
+
+                echo json_encode([
+                    'group'  => $this->request->getPost('group'),
+                    'pid'    => $pid_redirect,
+                    'status' => TRUE
+                ]);
+            }
+        }
+        else
+        {
+            throw \CodeIgniter\Exceptions\PageNotFoundException::forPageNotFound();
         }
     }
 }

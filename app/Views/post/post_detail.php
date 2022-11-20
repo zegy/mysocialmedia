6c2d77e02b232d62d4d6e38478d043bb46532e19
Zegy Sukmawan <zegy.sukmawan@gmail.com>
2022-11-20 21:28:01 +0700

    clean

--- app/Views/post/post_detail.php
+++ app/Views/post/post_detail.php
@@ -8,12 +8,6 @@
         <div class="col-sm-6">
           <h4><b>Judul Diskusi : </b><span id="post_judul"><?= $post->pttl ?></span></h4>
         </div>
-        <div class="col-sm-6">
-          <ol class="breadcrumb float-sm-right">
-            <li class="breadcrumb-item"><a href="#">Forum Diskusi : <?= $post->type ?></a></li>
-            <li class="breadcrumb-item active">No : <?= $post->pid ?></li>
-          </ol>
-        </div>
       </div>
     </div><!-- /.container-fluid -->
   </section>

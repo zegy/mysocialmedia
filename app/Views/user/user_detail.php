f1e9ddf1b976170061111acda3d1f41a291717ff
Zegy Sukmawan <zegy.sukmawan@gmail.com>
2022-11-24 22:08:27 +0700

    OTC user profile/detail form

--- app/Views/user/user_detail.php
+++ app/Views/user/user_detail.php
@@ -37,36 +37,91 @@
               <div class="tab-content">
                 <div class="active tab-pane" id="informasi">
                   <form class="form-horizontal">
-                    <div class="form-group row">
+                    
+                    <!-- <div class="form-group row">
                       <label for="inputName" class="col-sm-2 col-form-label">Name</label>
                       <div class="col-sm-10">
-                        <input type="email" class="form-control" id="inputName" placeholder="Name">
+                        <input type="text" class="form-control" id="inputName" placeholder="Name">
+                      </div>
+                    </div> -->
+
+                    <div class="form-group row">
+                      <label for="user_name" class="col-sm-2 col-form-label">user_name</label>
+                      <div class="col-sm-10">
+                        <input type="text" class="form-control" name="user_name" id="user_name">
                       </div>
                     </div>
+                    <div class="form-group row"><!-- NOTE DANGER : Check later -->
+                      <label for="user_password" class="col-sm-2 col-form-label">user_password</label>
+                      <div class="col-sm-10">
+                        <input type="password" class="form-control" name="user_password" id="user_password">
+                      </div>
+                    </div>
+                    <div class="form-group row"><!-- NOTE DANGER : Check later -->
+                      <label for="conf_user_password" class="col-sm-2 col-form-label">conf_user_password</label>
+                      <div class="col-sm-10">
+                        <input type="password" class="form-control" name="conf_user_password" id="conf_user_password">
+                      </div>
+                    </div>
+                    <div class="form-group row">
+                      <label for="user_full_name" class="col-sm-2 col-form-label">user_full_name</label>
+                      <div class="col-sm-10">
+                        <input type="text" class="form-control" name="user_full_name" id="user_full_name">
+                      </div>
+                    </div>
+                    <div class="form-group row">
+                      <label for="user_email" class="col-sm-2 col-form-label">user_email</label>
+                      <div class="col-sm-10">
+                        <input type="email" class="form-control" name="user_email" id="user_email">
+                      </div>
+                    </div>
+                    <div class="form-group row">
+                      <label for="user_tel" class="col-sm-2 col-form-label">user_tel</label>
+                      <div class="col-sm-10">
+                        <input type="tel" class="form-control" name="user_tel" id="user_tel"><!-- TODO : "type=tel"... Use pattern? -->
+                      </div>
+                    </div>
+
+                    <div class="form-group row">
+                      <label for="user_sex" class="col-sm-2 col-form-label">user_sex</label>
+                      <div class="col-sm-10">
+                        <select class="form-control" name="user_sex" id="user_sex">
+                          <option value="" selected>Select</option>
+                          <option value="m">Laki-laki</option>
+                          <option value="f">Perempuan</option>
+                        </select>
+                      </div>
+
+
+                      <!-- <label>user_sex</label>
+                      <select class="form-control" name="user_sex" id="user_sex">
+                        <option value="" selected>Select</option>
+                        <option value="m">Laki-laki</option>
+                        <option value="f">Perempuan</option>
+                      </select> -->
+                    </div>
+
+
+                    
+
+
+
+
+
                     <div class="form-group row">
                       <label for="inputEmail" class="col-sm-2 col-form-label">Email</label>
                       <div class="col-sm-10">
                         <input type="email" class="form-control" id="inputEmail" placeholder="Email">
                       </div>
                     </div>
-                    <div class="form-group row">
-                      <label for="inputName2" class="col-sm-2 col-form-label">Name</label>
-                      <div class="col-sm-10">
-                        <input type="text" class="form-control" id="inputName2" placeholder="Name">
-                      </div>
-                    </div>
+
                     <div class="form-group row">
                       <label for="inputExperience" class="col-sm-2 col-form-label">Experience</label>
                       <div class="col-sm-10">
                         <textarea class="form-control" id="inputExperience" placeholder="Experience"></textarea>
                       </div>
                     </div>
-                    <div class="form-group row">
-                      <label for="inputSkills" class="col-sm-2 col-form-label">Skills</label>
-                      <div class="col-sm-10">
-                        <input type="text" class="form-control" id="inputSkills" placeholder="Skills">
-                      </div>
-                    </div>
+                    
                     <div class="form-group row">
                       <div class="offset-sm-2 col-sm-10">
                         <div class="checkbox">
@@ -76,11 +131,13 @@
                         </div>
                       </div>
                     </div>
+
                     <div class="form-group row">
                       <div class="offset-sm-2 col-sm-10">
                         <button type="submit" class="btn btn-success">Update</button>
                       </div>
                     </div>
+                    
                   </form>
                   <div class="form-group row">
                     <div class="col" >

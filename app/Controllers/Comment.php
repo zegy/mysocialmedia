9f8389d48cd4d7ce56c87d6bbd231c89ee3fa08c
Zegy Sukmawan <zegy.sukmawan@gmail.com>
2022-12-05 15:00:46 +0700

    Add FCM (README)
    using php's curl, not ci's. Clean ASAP

--- app/Controllers/Comment.php
+++ app/Controllers/Comment.php
@@ -66,6 +66,33 @@ class Comment extends BaseController
             $output = ['status' => true];
         }
 
+        // FCM START
+        // Using PHP's "cURL Functions". Failed to use CI's "CURLRequest" Class. Ref : part-1-experimental AND https://shareurcodes.com/blog/send%20push%20notification%20to%20users%20using%20firebase%20messaging%20service%20in%20php
+        $fields = [
+            "notification" => [
+                "body"         => 'Ada yang mengomentari postingan anda!',
+                "title"        => 'DIPSI',
+                "icon"         => 'icon',
+                "click_action" => base_url('group/umum')
+            ],
+            "to" => "eU-gPExR5cIfiVFV4Jwie4:APA91bHfXkAYjxRww2dGhDxGkQlGjL5yqdhpBe4aofHTQ2Vm5lpo9utrk-s4NOlqtuojIKH3yMPNRmGCMO_BqxM_axH2MBY8NWzbFL8GQBnRsBK5e_8Hs39JA06qkofeyvb98M4dSKI9"
+        ];
+
+        $headers = [ // Danger! Notice it's not an associative array!
+            'Authorization: key=AAAArTff7d4:APA91bFQArT9HUGgZtPk7EDV3pFKX3DozsU4_qy8mSLZ1VYzgufVrTJN_h627bVzhm4Izq4Bu8PLpllmg8CCW-EjU8XKzoW_LvoypqFi7I_jUyUGk3gwnh_CwPapF-_oa_FKRZQ9Mlxn',
+            'Content-Type: application/json'
+        ];
+
+        $ch = curl_init();
+        curl_setopt($ch,CURLOPT_URL,'https://fcm.googleapis.com/fcm/send');
+        curl_setopt($ch,CURLOPT_POST,true);
+        curl_setopt($ch,CURLOPT_HTTPHEADER,$headers);
+        curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+        curl_setopt($ch,CURLOPT_POSTFIELDS,json_encode($fields));
+        curl_exec($ch);
+        curl_close($ch);
+        // FCM END
+
         echo json_encode($output);
     }
 

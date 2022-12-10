5b4b4d318cd99fb5d201b9bb37e68eb3cf5ec37b
Zegy Sukmawan <zegy.sukmawan@gmail.com>
2022-12-10 20:12:12 +0700

    VU to sync prev

--- app/Views/notif/notif_list.php
+++ app/Views/notif/notif_list.php
@@ -6,9 +6,9 @@
       <h3 class="dropdown-item-title">
         <?= $notif->from_user_full_name ?>
         <!-- <span class="float-right text-sm text-danger"><i class="fas fa-star"></i></span> NOTE Original!-->
-        <span class="float-right text-sm text-danger"><?php if ($notif->notif_type == 'comment') {echo('Comment');} ?></span> <!-- NOTE The custom -->
+        <span class="float-right text-sm text-danger"><?php if ($notif->notif_type == 'comment') {echo('Komentar');} else {echo('Komentar');} ?></span> <!-- NOTE The custom -->
       </h3>
-      <p class="text-sm"><?php if ($notif->notif_type == 'comment') {echo('Telah mengomentari postingan anda!');} ?></p>
+      <p class="text-sm"><?php if ($notif->notif_type == 'comment') {echo('telah mengomentari postingan anda!');} else {echo('mengomentari postingan yang anda ikuti!!');} ?></p>
       <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i><?= $notif->notif_date_time ?></p>
     </div>
   </div><!-- Message End -->

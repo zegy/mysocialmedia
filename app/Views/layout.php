fb757d33626ff671b39204fe75feec699661fba0
Zegy Sukmawan <zegy.sukmawan@gmail.com>
2022-12-13 15:32:05 +0700

    C FCM

--- app/Views/layout.php
+++ app/Views/layout.php
@@ -181,14 +181,8 @@
     // Retrieve Firebase Messaging object.
     const messaging = firebase.messaging();
   
-    // Handle incoming messages. Called when:
-    // - a message is received while the app has focus
-    // - the user clicks on an app notification created by a service worker
-    //   `messaging.onBackgroundMessage` handler.
     messaging.onMessage((payload) => {
       console.log('Message received. ', payload);
-      // Update the UI to include the received message.
-    //   appendMessage(payload);
       get_notif_list()
     });
   </script>
